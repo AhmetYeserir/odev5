9798b7877b80c182e74cd7f1e9048e2947b824e3
Ahmet Yeşeriroğlu <ahmetgs131@gmail.com>
2022-10-24 22:13:58 +0300

    Proje dosyası ekle.

---
+++ odev5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1.sayıyı giriniz : ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2.sayıyı giriniz : ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            if (sayi1 % sayi2 == 0)
+            {
+                Console.WriteLine("Birbirlerine tam bölünebilirler");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Birbilerine tam bölünemezler");
+                Console.ReadLine();
+            }
+        }
+    }
+}
